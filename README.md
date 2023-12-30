535d95acb6b7a6badbdaf0e629cfd19de9fdf233
VermiIIi0n <104612478+VermiIIi0n@users.noreply.github.com>
2023-12-30 16:34:10 +0800

    add more performant property

--- README.md
+++ README.md
@@ -8,22 +8,36 @@ A simple property implementation for C++
 
 ```cpp
     #include "property.hpp"
+    using namespace vermils::tricks
 
     class Foo {
         int _bar=0;
         public:
-            Property<int> bar({
+            Property<int> bar{
                 [this] () -> int { return this->_bar; },
                 [this] (int value) { this->_bar = value; }
-            });
+            };
         });
+
+        // or use more a performant version, but is much less elegant
+        struct Bar : StaticProperty<int, Foo&>
+        {
+            Bar(Foo& foo) : StaticProperty<int, Foo&>(foo) {}
+            using StaticProperty<int, Foo&>::operator=;
+            void setter(int value) const override { this->value = value; }
+            int getter() const override { return this->value; }
+        }
+
+        Bar static_bar{*this};
     };
 
     int main()
     {
         Foo foo;
         foo.bar = 1;
+        foo.static_bar = 2;
         std::cout << foo.bar << std::endl; // 1
+        std::cout << foo.static_bar << std::endl; // 2
         return 0;
     }
 ```
--- include/property.hpp
+++ include/property.hpp
@@ -1,81 +1,158 @@
 #pragma once
 
 #include <functional>
+#include <stdexcept>
 
-namespace ppp
+namespace vermils
 {
-    /**
-     * @brief Property Class
-     *
-     * @tparam T the type of the property
-     * @param getter const std::function<T()> getter
-     * @param setter const std::function<void(const T)> setter
-     */
-    template <typename T>
-    class Property
+namespace tricks
+{
+
+template <typename T, typename O>
+struct StaticReadOnlyProperty
+{
+protected:
+    O &&owner;
+public:
+    constexpr StaticReadOnlyProperty(O &&owner) : owner(owner) {}
+    virtual ~StaticReadOnlyProperty() = default;
+    virtual T getter() const = 0;
+    operator T() const
     {
-    protected:
-        const std::function<T()> getter;
-        const std::function<void(const T)> setter;
-
-    public:
-        typedef std::function<T()> Getter;
-        typedef std::function<void(const T)> Setter;
-        Property() = delete;
-        Property(std::function<T()> getter, std::function<void(const T)> setter = nullptr)
-            : getter(getter), setter(setter)
-        {
-        }
-
-        operator T() const
-        {
-            return getter();
-        }
-
-        T operator ()() const
-        {
-            return getter();
-        }
-
-        T operator=(const T value)
-        {
-            if (!setter)
-                throw std::runtime_error("Property setter is not defined");
-            setter(value);
-            return value;
-        }
-    };
-
-    template <typename T>
-    class Property<T &>
+        return getter();
+    }
+    T operator ()() const
     {
-    protected:
-        const std::function<T()> getter;
-        const std::function<void(const T &)> setter;
+        return static_cast<T>(*this);
+    }
+};
 
-    public:
-        Property(std::function<T()> getter, std::function<void(const T &)> setter = nullptr)
-            : getter(getter), setter(setter)
-        {
-        }
+template <typename T, typename O>
+struct StaticProperty : StaticReadOnlyProperty<T, O>
+{
+public:
+    constexpr StaticProperty(O &&owner) : StaticReadOnlyProperty<T, O>(std::forward<O>(owner)) {}
+    virtual void setter(T value) const {};
+    T operator=(T value) const
+    {
+        setter(value);
+        return value;
+    }
+};
 
-        operator T() const
-        {
-            return getter();
-        }
+template <typename T, typename O>
+struct StaticReadOnlyProperty<T &, O>
+{
+protected:
+    O &&owner;
+public:
+    constexpr StaticReadOnlyProperty(O &&owner) : owner(owner) {}
+    virtual ~StaticReadOnlyProperty() = default;
+    virtual T& getter() const = 0;
+    operator T&() const
+    {
+        return getter();
+    }
+    T &operator ()() const
+    {
+        return static_cast<T>(*this);
+    }
+};
 
-        T& operator ()() const
-        {
-            return getter();
-        }
+template <typename T, typename O>
+struct StaticProperty<T &, O> : StaticReadOnlyProperty<T &, O>
+{
+public:
+    constexpr StaticProperty(O &&owner) : StaticReadOnlyProperty<T &, O>(std::forward<O>(owner)) {}
+    virtual void setter(T &&value) const {};
+    T &operator=(T &&value) const
+    {
+        setter(std::forward<T>(value));
+        return value;
+    }
+};
 
-        const T &operator=(const T &value)
-        {
-            if (!setter)
-                throw std::runtime_error("Property setter is not defined");
-            setter(value);
-            return value;
-        }
-    };
+/**
+ * @brief Property Class
+ *
+ * @tparam T the type of the property
+ * @param getter const std::function<T()> getter
+ * @param setter const std::function<void(const T)> setter
+ */
+template <typename T>
+class Property
+{
+protected:
+    const std::function<T()> getter;
+    const std::function<void(const T)> setter;
 
+public:
+    typedef std::function<T()> Getter;
+    typedef std::function<void(const T)> Setter;
+    Property() = delete;
+    Property(std::function<T()> getter, std::function<void(const T)> setter = nullptr)
+        : getter(getter), setter(setter)
+    {
+    }
+
+    operator T() const
+    {
+        return getter();
+    }
+
+    T operator ()() const
+    {
+        return getter();
+    }
+
+    T operator=(const T value)
+    {
+        if (!setter)
+            throw std::runtime_error("Property setter is not defined");
+        setter(value);
+        return value;
+    }
+};
+
+template <typename T>
+class Property<T &>
+{
+protected:
+    const std::function<T()> getter;
+    const std::function<void(T &&)> setter;
+
+public:
+    Property(std::function<T()> getter, std::function<void(T &&)> setter = nullptr)
+        : getter(getter), setter(setter)
+    {
+    }
+
+    operator T() const
+    {
+        return getter();
+    }
+
+    T& operator ()() const
+    {
+        return getter();
+    }
+
+    T &operator=(const T &value)
+    {
+        if (!setter)
+            throw std::runtime_error("Property setter is not defined");
+        setter(value);
+        return value;
+    }
+
+    T &operator=(T &&value)
+    {
+        if (!setter)
+            throw std::runtime_error("Property setter is not defined");
+        setter(std::forward<T>(value));
+        return getter();
+    }
+};
+
+}
 }
